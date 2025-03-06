fe9ced79bdb119dab5b8758d03f00d96fbacc397
Lubos Balat <lubos.balat@interway.sk>
2025-03-06 12:48:19 +0100

    feat: add GitHub deployment target

--- ant/deploy.sh
+++ ant/deploy.sh
@@ -21,6 +21,7 @@ ant update-version
 #ant deploy
 ant -Dcompress=true createUpdateZip
 ant -Dcompress=true createUpdateZipJar
+ant deployGithub
 
 #GITHUB: just run ant deployStaging
 #then close repo and deploy it
