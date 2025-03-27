0738c6d58ebc891b82953137fc97ed1539dd6b9c
Lubos Balat <lubos.balat@interway.sk>
2025-03-27 14:11:12 +0100

    [57673] feat: setup screenshots

--- src/test/webapp/screenshots/generator/setup.js
+++ src/test/webapp/screenshots/generator/setup.js
@@ -6,11 +6,22 @@ Before(({ I }) => {
     confLng = I.getConfLng();
 });
 
+/*
+POZOR: tieto testy je potrebne vykonavat postupne cez @current po jednom scenari a zakazdym restartovat server.
+Kod ale obsahuje pause(), takze da sa pustit aj naraz a pri kazdom pause len spravit restart app servera.
+
+Execute SQL to create blank database and connect to it in poolman-local.xml:
+DROP DATABASE IF EXISTS blank_web_2;
+CREATE DATABASE blank_web_2 DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
+*/
+
+
 Scenario('Setup action', ({I, Document}) => {
 
     I.say("POZOR ! POZOR ! POZOR ! POZOR ! POZOR ! POZOR ! POZOR ! POZOR !");
     I.say("Databaza musí byť prázdna !!! Nesmie obsahovať žiadne tabuľky");
     I.say("-----------------------------------------------------------------");
+    pause();
 
     I.amOnPage("/admin");
     Document.screenshot("/install/setup/error.png", 660, 200);
@@ -25,7 +36,7 @@ Scenario('Setup action', ({I, Document}) => {
     I.fillField("#conf_installName", "webjetcms");
     I.fillField("#conf_license", "");
 
-    Document.screenshot("/install/setup/setup.png", 660, 520);
+    Document.screenshot("/install/setup/setup.png", 900, 1040);
 
     switch (confLng) {
         case 'sk':
@@ -53,15 +64,16 @@ Scenario('Setup action', ({I, Document}) => {
         I.waitForText("WebJET je nakonfigurován");
     }
 
-    Document.screenshot("/install/setup/setup-saved.png", 660, 180);
+    Document.screenshot("/install/setup/setup-saved.png", 900, 400);
 
 });
 
 Scenario('First login/password change /and others', ({I, DTE, Document}) => {
     I.say("POZOR ! POZOR ! POZOR ! POZOR ! POZOR ! POZOR ! POZOR ! POZOR !");
     I.say("Tento test musí byť vykonaný po inicializácií DB (setup akcie) a po následnom reštartovaní");
-    I.say("WebJET musí byť naštartovaný už aj s licenciou !!");
+    I.say("WebJET musí byť naštartovaný !!");
     I.say("-----------------------------------------------------------------");
+    pause();
 
     I.amOnPage("/admin/logon/");
 
@@ -91,7 +103,7 @@ Scenario('First login/password change /and others', ({I, DTE, Document}) => {
     I.fillField("#newPassword", secret(I.getDefaultPassword()));
     I.fillField("#retypeNewPassword", secret(I.getDefaultPassword()));
 
-    Document.screenshot("/install/setup/change-password.png");
+    Document.screenshot("/install/setup/change-password.png", 1280, 950);
 
     I.clickCss("#login-submit");
 
@@ -112,9 +124,40 @@ Scenario('First login/password change /and others', ({I, DTE, Document}) => {
     I.click('//*[@id="perms_conf.show_all_variables_anchor"]/i[1]');
 
     Document.screenshot("/install/setup/user-perms.png");
+
+    DTE.save();
+});
+
+Scenario('License expiration notification test', ({I, Document}) => {
+    I.relogin("ADMIN");
+
+    let actualDate = new Date();
+    let monthMillis = 30 * 24 * 60 * 60 * 1000;
+
+    I.say("Set license expiration date to 1 month from now");
+    Document.setConfigValue("licenseExpiryDate", actualDate.getTime() - monthMillis);
+
+    I.amOnPage("/admin/v9/");
+    I.waitForElement("#toast-container-overview > div.toast.toast-warning", 5);
+
+    Document.screenshot("/install/license/license-expiration-notification.png", 1200, 300);
+
+    I.say("Set license expiration date to value 0");
+    Document.setConfigValue("licenseExpiryDate", 0);
 });
 
 Scenario('Pridanie/zmena licencie', ({I, Document}) => {
+
+    I.say("POZOR ! POZOR ! POZOR ! POZOR ! POZOR ! POZOR ! POZOR ! POZOR !");
+    I.say("Tento test vykonaj ta, že zadaj sasdfadsfasdf (neplatné licenčné číslo, min. 10 znakov) do konf. premennej license a reštartuj WebJET");
+    I.say("Prihlasujes sa ako ADMIN ale s heslom testera");
+    I.say("-----------------------------------------------------------------");
+
+    I.relogin("ADMIN");
+    Document.setConfigValue("license", "sasdfadsfasdf");
+    I.logout();
+
+    pause();
 
     switch (confLng) {
         case 'sk':
@@ -134,7 +177,7 @@ Scenario('Pridanie/zmena licencie', ({I, Document}) => {
     I.fillField("#username", "admin");
 
     I.say("Zadaj heslo a licencne cislo");
-    pause();
+    I.fillField("#password", secret(I.getDefaultPassword()));
 
     I.clickCss("#btnOk");
 
@@ -156,20 +199,3 @@ Scenario('Pridanie/zmena licencie', ({I, Document}) => {
     Document.screenshot("/install/license/license-saved.png");
 });
 
-Scenario('License expiration notification test', ({I, login, Document}) => {
-    login("admin");
-
-    let actualDate = new Date();
-    let monthMillis = 30 * 24 * 60 * 60 * 1000;
-
-    I.say("Set license expiration date to 1 month from now");
-    Document.setConfigValue("licenseExpiryDate", actualDate.getTime() - monthMillis);
-
-    I.amOnPage("/admin/v9/");
-    I.waitForElement("#toast-container-overview > div.toast.toast-warning", 5);
-
-    Document.screenshot("/install/license/license-expiration-notification.png", 1200, 300);
-
-    I.say("Set license expiration date to value 0");
-    Document.setConfigValue("licenseExpiryDate", 0);
-});
