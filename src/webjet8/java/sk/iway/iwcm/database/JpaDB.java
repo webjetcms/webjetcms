4ac2739c5be4971882138b4edee9900e37615d7c
Lubos Balat <lubos.balat@interway.sk>
2025-04-24 17:16:40 +0200

    [56845] fix: tests

--- src/webjet8/java/sk/iway/iwcm/database/JpaDB.java
+++ src/webjet8/java/sk/iway/iwcm/database/JpaDB.java
@@ -74,19 +74,19 @@ public class JpaDB<T extends ActiveRecordBase>
 		T obj;
 		Long idLong = null;
 		//pre ActiveRecordRepository musime posielat ID ako long
-        Integer idToUse = Integer.valueOf(id.intValue());
+        Integer idInt = Integer.valueOf(id.intValue());
 		if (clazz.getSuperclass().isAssignableFrom(ActiveRecordRepository.class)) idLong = Long.valueOf(id);
 
 		if((InitServlet.isTypeCloud() || Constants.getBoolean("enableStaticFilesExternalDir")) && Constants.getString("jpaFilterByDomainIdBeanList").indexOf(clazz.getName()) != -1)
 		{
 			Map<String,Object>  hashMap = new HashMap<String,Object>();
 			hashMap.put("domainId", CloudToolsForCore.getDomainId());
-			if (idLong == null) obj = em.find(clazz, idToUse, hashMap);
+			if (idLong == null) obj = em.find(clazz, idInt, hashMap);
 			else obj = em.find(clazz, idLong, hashMap);
 		}
 		else
 		{
-			if (idLong == null) obj = em.find(clazz, idToUse);
+			if (idLong == null) obj = em.find(clazz, idInt);
 			else obj = em.find(clazz, idLong);
 		}
 
