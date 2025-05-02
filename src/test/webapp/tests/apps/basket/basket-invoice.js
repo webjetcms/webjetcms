82cf6d149cf651f1eed5da78d4b8f27cd1dafbb1
Lubos Balat <lubos.balat@interway.sk>
2025-05-02 15:23:12 +0200

    Merge branch 'feature/56609-eshop-sposob-platby'

--- src/test/webapp/tests/apps/basket/basket-invoice.js
+++ src/test/webapp/tests/apps/basket/basket-invoice.js
@@ -68,7 +68,6 @@ Scenario('Eshop invoice tests', async ({I, DT, DTE, Document}) => {
 
     I.say("Check payment tab - inner table");
         I.clickCss("#pills-dt-basketInvoiceDataTable-payments-tab");
-        I.seeElement( locate("h5").withText("Prehľad platieb") );
         I.seeElement( locate("div.col-sum").withText("Zaplatená suma: 0,00 eur zo sumy: 3 126,36 eur") );
 
         I.say("Check inner table functionality + footer update");
@@ -84,7 +83,6 @@ Scenario('Eshop invoice tests', async ({I, DT, DTE, Document}) => {
 
     I.say("Check items tab - inner table");
         I.clickCss("#pills-dt-basketInvoiceDataTable-items-tab");
-        I.seeElement( locate("h5").withText("Prehľad položiek objednávky") );
         I.seeElement( locate("div.col-sum").withText("Suma k zaplateniu: 3 126,36 eur") );
 
             I.say("Check buttons visibility");
