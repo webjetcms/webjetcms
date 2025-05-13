44d556b9141e191c2454d7a7f209126520c78fe7
jmisovsky <jakub.misovsky@interway.sk>
2025-05-13 10:26:58 +0200

    [57785] REMOVE abundant await/async

--- src/test/webapp/screenshots/generator/admin/password-reset.js
+++ src/test/webapp/screenshots/generator/admin/password-reset.js
@@ -2,18 +2,18 @@ Feature('password-recovery');
 
 var basePath = "/redactor/admin/password-recovery/"
 
-Scenario('admin-password-recovery', async ({ I, Document, TempMail }) => {
+Scenario('admin-password-recovery', ({ I, Document, TempMail }) => {
     I.logout();
-    await doScreens(I, Document, TempMail, true);
+    doScreens(I, Document, TempMail, true);
 });
 
 Scenario('delete cache objects to prevent logon form wrong password counting, 4 @singlethread', ({ I, Document }) => {
     Document.deleteAllCacheObjects();
 });
 
-Scenario('user-password-recovery', async ({ I, Document, TempMail }) => {
+Scenario('user-password-recovery', ({ I, Document, TempMail }) => {
     I.wait(11);
-    await doScreens(I, Document, TempMail, false);
+    doScreens(I, Document, TempMail, false);
 });
 
 Scenario('password-recovery remove emails', async ({ TempMail }) => {
@@ -21,7 +21,7 @@ Scenario('password-recovery remove emails', async ({ TempMail }) => {
     await TempMail.destroyInbox();
 });
 
-async function doScreens(I, Document, TempMail, isAdmin) {
+function doScreens(I, Document, TempMail, isAdmin) {
 
     let screensPrefix;
     let lostPasswordBtn;
@@ -47,24 +47,24 @@ async function doScreens(I, Document, TempMail, isAdmin) {
     I.amOnPage(logonLink);
     I.waitForVisible(lostPasswordBtn);
 
-    await Document.screenshotElement(lostPasswordBtn, basePath + screensPrefix + "-recover-password-btn.png");
+    Document.screenshotElement(lostPasswordBtn, basePath + screensPrefix + "-recover-password-btn.png");
 
     I.clickCss(lostPasswordBtn);
     I.wait(1);
 
-    await Document.screenshot(basePath + screensPrefix + "-recovery-page.png", 1080, 685);
-    await Document.screenshotElement("#register-submit-btn", basePath + screensPrefix + "-send-btn.png");
+    Document.screenshot(basePath + screensPrefix + "-recovery-page.png", 1080, 685);
+    Document.screenshotElement("#register-submit-btn", basePath + screensPrefix + "-send-btn.png");
 
     I.fillField('input[name="loginName"]', "sameMail@fexpost.com");
     I.clickCss('button#register-submit-btn');
     I.see('Ak Vaše konto existuje heslo Vám bolo zaslané na e-mailovú adresu.');
 
-    await Document.screenshot(basePath + screensPrefix + "-recovery-page-notif.png", 1080, 685);
+    Document.screenshot(basePath + screensPrefix + "-recovery-page-notif.png", 1080, 685);
 
     TempMail.login("sameMail");
     TempMail.openLatestEmail();
 
-    await Document.screenshotElement("#info", basePath + "email.png");
+    Document.screenshotElement("#info", basePath + "email.png");
 
     I.say("Go change password");
     I.click("Ak si chcete zmeniť heslo, kliknite sem do 30 minút.");
@@ -72,26 +72,26 @@ async function doScreens(I, Document, TempMail, isAdmin) {
 
     I.fillField('input[type="password"][name="newPassword"]', "asd");
     I.fillField(retypeInput, "asddd");
-    await Document.screenshotElement(".container", basePath + screensPrefix + "-recovery-form-1.png");
+    Document.screenshotElement(".container", basePath + screensPrefix + "-recovery-form-1.png");
 
     I.clickCss(submitBtn);
-    await Document.screenshotElement(".container", basePath + screensPrefix + "-recovery-form-2.png");
+    Document.screenshotElement(".container", basePath + screensPrefix + "-recovery-form-2.png");
 
     I.fillField('input[type="password"][name="newPassword"]', "asd");
     I.fillField(retypeInput, "asd");
     I.clickCss(submitBtn);
-    await Document.screenshotElement(".container", basePath + screensPrefix + "-recovery-form-3.png");
+    Document.screenshotElement(".container", basePath + screensPrefix + "-recovery-form-3.png");
 
     const randomPassword = 'password_P' + I.getRandomText();
     I.fillField('input[type="password"][name="newPassword"]', randomPassword);
     I.fillField(retypeInput, randomPassword);
     I.clickCss(submitBtn);
-    await Document.screenshot(".container", basePath + screensPrefix + "-recovery-form-4.png");
+    Document.screenshot(".container", basePath + screensPrefix + "-recovery-form-4.png");
 
     I.closeCurrentTab();
     I.click("Ak si chcete zmeniť heslo, kliknite sem do 30 minút.");
     I.switchToNextTab();
-    await Document.screenshot(basePath + screensPrefix + "-recovery-form-notWorking.png", 700, 400);
+    Document.screenshot(basePath + screensPrefix + "-recovery-form-notWorking.png", 700, 400);
 
     I.say("Ask for change again and cancel it");
     I.wait(11);
@@ -110,5 +110,5 @@ async function doScreens(I, Document, TempMail, isAdmin) {
     I.click("Ak ste nepožiadali o zmenu hesla môžete túto akciu zrušiť kliknutím sem.");
     I.switchToNextTab();
 
-    await Document.screenshot(basePath + screensPrefix + "-recovery-form-cancel.png", 700, 400);
+    Document.screenshot(basePath + screensPrefix + "-recovery-form-cancel.png", 700, 400);
 }
