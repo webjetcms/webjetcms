06d04860a7ec7e13a69258240f1c2b96d92f2d6c
jmisovsky <jakub.misovsky@interway.sk>
2025-05-13 10:23:00 +0200

    [57785] FIX password-recovery screenshots

--- src/test/webapp/screenshots/generator/manual-redactor.js
+++ src/test/webapp/screenshots/generator/manual-redactor.js
@@ -398,6 +398,7 @@ Scenario('apps-qa', ({ I, DT, DTE, Document }) => {
 Scenario('logon', ({ I, Document, i18n }) => {
     I.amOnPage('/logoff.do?forward=/admin/');
     I.amOnPage('/admin/logon/?language='+I.getConfLng()+'&id=2');
+    I.selectOption("#language", i18n.get("English"));
 
     I.fillField("#password", "12345");
     Document.screenshot("/redactor/admin/logon.png", 1080, 685);
