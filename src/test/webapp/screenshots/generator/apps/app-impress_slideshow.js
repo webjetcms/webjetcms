8073bc170f306f356baa09d1beb056434d6ae60d
sivan <sebastian.ivan@interway.sk>
2025-06-24 16:22:23 +0200

    [57409] impress slideshow app doc

--- src/test/webapp/screenshots/generator/apps/app-impress_slideshow.js
+++ src/test/webapp/screenshots/generator/apps/app-impress_slideshow.js
@@ -4,14 +4,29 @@ Before(({ login }) => {
     login('admin');
 });
 
-Scenario('posobiva prezentacia', ({ I, DT, DTE, Document }) => {
+Scenario('impress slideshow app screens', ({ I, DT, DTE, Document }) => {
     I.amOnPage("/apps/posobiva-prezentacia/");
     DT.waitForLoader();
     Document.screenshot("/redactor/apps/app-impress_slideshow/app-impress_slideshow.png");
 
     Document.screenshotAppEditor(77868, "/redactor/apps/app-impress_slideshow/editor-style.png", function(Document, I, DT, DTE) {
-        I.clickCss("#tabLink2");
+        I.clickCss("#pills-dt-component-datatable-tabLink2-tab");
         Document.screenshot("/redactor/apps/app-impress_slideshow/editor-items.png");
-        I.clickCss("#tabLink1");
+
+        I.waitForVisible("#DTE_Field_iframe", 5);
+        I.switchTo("#DTE_Field_iframe");
+
+        I.clickCss("td.sorting_1");
+        I.clickCss("button.buttons-edit");
+        DTE.waitForEditor("impressSlideshowDataTable");
+
+        Document.screenshot("/redactor/apps/app-impress_slideshow/editor-items-edit.png");
+
+        DTE.cancel();
+
+        I.switchTo();
+        I.switchTo('.cke_dialog_ui_iframe');
+        I.switchTo('#editorComponent');
+        I.clickCss("#pills-dt-component-datatable-style-tab");
     });
 });
