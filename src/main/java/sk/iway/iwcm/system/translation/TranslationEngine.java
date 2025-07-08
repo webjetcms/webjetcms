a0055a6f30a491ebd69800bf7568606970623980
SebastianIvanInterway <150778792+SebastianIvanInterway@users.noreply.github.com>
2025-07-08 15:34:12 +0200

    [57881] support multi translation engine definition + engine info (#52)
    * [57881] supprot multi translation engine definition + engine info
    
    * [57881] Add translation model_type, fix bug + optimalization
    
    * [57881] Clone / MIrror logic FIX
    
    * [57881] enable undo groups/docs sync
    
    * [57881] docs + screen + edit
    
    * [57881] docs
    
    * [57881] improvements
    
    * [57881] enhance
    
    * [57881] code review
    
    * Build 2025.0-SNAPSHOT.020 03.07.2025 15:25:18
    
    * [57881] chore: code review
    
    * [57881] chore: code review
    
    * [57881] chore: code review
    
    * [57881] fix: mirroring - move existing folder into another one
    
    * [57881] fix: mirroring - move existing folder into another one
    
    ---------
    
    Co-authored-by: Lubos Balat <lubos.balat@interway.sk>

---
+++ src/main/java/sk/iway/iwcm/system/translation/TranslationEngine.java
@@ -0,0 +1,50 @@
+package sk.iway.iwcm.system.translation;
+
+import java.util.Random;
+
+import sk.iway.iwcm.Logger;
+
+public abstract class TranslationEngine {
+
+    protected static final int MAX_RETRIES = 5;
+    protected static final int BASE_DELAY_MS = 1000;
+    protected static final Random random = new Random();
+
+    public abstract String engineName();
+    public abstract Long numberOfFreeCharacters();
+    public abstract String translate(String text, String fromLanguage, String toLanguage);
+    public abstract boolean isConfigured();
+
+    /**
+     * Compute exponential delay based on attempt number. Use delay ms to fall asleep thread.
+     * @param attempt - number of attemp
+     * @return TRUE if sleep was successfull and we have another try, else FALSE
+     */
+    protected boolean applyDelay(int attempt) {
+        try {
+            //Too many requests, slow down
+            int exponentialDelay = BASE_DELAY_MS * (1 << attempt); // 2^attempt * base
+            // Add jitter: +/- 0-1000 ms
+            exponentialDelay = exponentialDelay + random.nextInt(1000);
+
+            Logger.debug(TranslationEngine.class, "Too many requests error. Attempt number : " + (attempt + 1) + ", waiting for for : " + exponentialDelay + " ms");
+            Thread.sleep(exponentialDelay);
+        } catch (Exception ex2) {
+            sk.iway.iwcm.Logger.error(ex2);
+            return false;
+        }
+
+        // Waiting done
+        Logger.debug(TranslationEngine.class, "Waiting done");
+        // Increment attempt count
+        attempt++;
+
+        if(attempt >= MAX_RETRIES) {
+            Logger.error(TranslationEngine.class, "Unable to call API again, reached maximum number of attempts");
+            return false;
+        }
+
+        // We can try again
+        return true;
+    }
+}
--- src/test/java/sk/iway/iwcm/system/translation/TestTranslationService.java
+++ src/test/java/sk/iway/iwcm/system/translation/TestTranslationService.java
@@ -18,11 +18,11 @@ public class TestTranslationService extends BaseWebjetTest {
         DBPool.getInstance();
         DBPool.jpaInitialize();
     }
-    
+
     @Test
     public void checkDeeplTranslation() {
-        String inputTextSk = "Toto je test : !INCLUDE(pes.jsp, ja, som, najlepší)! , tento test je povinný. Aj tento by mal fungovať : !INCLUDE(neviem.jsp, skúsim, to, znovu)! ale neviem to zaručiť.";
-        String requiredOutputEn = "This is a test : !INCLUDE(pes.jsp, ja, som, najlepší)! , this test is mandatory. Also this one should work : !INCLUDE(neviem.jsp, skúsim, to, znovu)! but I can't guarantee it.";
+        String inputTextSk = "Toto je test: !INCLUDE(pes.jsp, ja, som, najlepší)!, tento test je povinný. Aj tento by mal fungovať: !INCLUDE(neviem.jsp, skúsim, to, znovu)!, ale neviem to zaručiť.";
+        String requiredOutputEn = "This is a test: !INCLUDE(pes.jsp, ja, som, najlepší)!, this test is mandatory. This one should also work: !INCLUDE(neviem.jsp, skúsim, to, znovu)!, but I cannot guarantee it.";
 
         // Init Translate from SK to EN
         TranslationService translator = new TranslationService("SK", "EN");
