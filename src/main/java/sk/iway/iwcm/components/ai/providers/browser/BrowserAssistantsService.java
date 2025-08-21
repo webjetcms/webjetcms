b3c37361b503a0f5b6085d2690efa592f4a85ac7
sivan <sebastian.ivan@interway.sk>
2025-08-21 14:49:46 +0200

    [57997] Resolve merge problem

--- src/main/java/sk/iway/iwcm/components/ai/providers/browser/BrowserAssistantsService.java
+++ src/main/java/sk/iway/iwcm/components/ai/providers/browser/BrowserAssistantsService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import sk.iway.iwcm.components.ai.jpa.AssistantDefinitionEntity;
 import sk.iway.iwcm.components.ai.providers.AiAssitantsInterface;
 import sk.iway.iwcm.i18n.Prop;
+import sk.iway.iwcm.system.datatable.DatatablePageImpl;
 
 /**
  * Service for Chrome Built-in AI:
@@ -44,6 +45,19 @@ public class BrowserAssistantsService implements AiAssitantsInterface {
     @Override
     public boolean isInit() {
         return true;
+    }
+
+    public List<String> getFieldsToShow(String action) {
+        //Local assistant do not need specific fields, base are enought
+        return new ArrayList<>();
+    }
+
+    public void prepareBeforeSave(AssistantDefinitionEntity assistantEnity) {
+        //no checks  needed for now
+    }
+
+    public void setProviderSpecificOptions(DatatablePageImpl<AssistantDefinitionEntity> page, Prop prop) {
+        //Local provider do not have specific fields that need. set options
     }
 
 }
