141e314f61d20aa6bf41c3dcf708ee6f3538c3b6
Lubos Balat <lubos.balat@interway.sk>
2025-11-06 09:21:51 +0100

    feat: add navigation transition

--- src/main/java/sk/iway/iwcm/components/basket/rest/ProductListService.java
+++ src/main/java/sk/iway/iwcm/components/basket/rest/ProductListService.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
