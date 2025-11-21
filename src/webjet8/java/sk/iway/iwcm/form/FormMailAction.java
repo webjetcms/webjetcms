4b2a97cce6756f4178c06c56ae4e746eff0ae969
Lubos Balat <lubos.balat@interway.sk>
2025-11-21 14:39:30 +0100

    [58149] revert: changes

--- src/webjet8/java/sk/iway/iwcm/form/FormMailAction.java
+++ src/webjet8/java/sk/iway/iwcm/form/FormMailAction.java
@@ -485,12 +485,17 @@ public class FormMailAction extends HttpServlet
 				}
 			}
 			iLastDocIdMail = iLastDocId;
-
 			//aby sme pre mail mohli specifikovat inu stranku ako to co sa zobrazuje
-			if (Tools.isEmpty(request.getParameter("useFormMailDocId")) || "none".equals(request.getParameter("useFormMailDocId"))) {
-				iLastDocIdMail = null;
-			} else {
-				iLastDocIdMail = Integer.valueOf(Tools.getIntValue(request.getParameter("useFormMailDocId"), -1));
+			if (request.getParameter("useFormMailDocId")!=null)
+			{
+				if ("none".equals(request.getParameter("useFormMailDocId")))
+				{
+					iLastDocIdMail = null;
+				}
+				else
+				{
+					iLastDocIdMail = Integer.valueOf(Tools.getIntValue(request.getParameter("useFormMailDocId"), -1));
+				}
 			}
 			Logger.debug(FormMailAction.class, "iLastDocId="+iLastDocId);
 		}
