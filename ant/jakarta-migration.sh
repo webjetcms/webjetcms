d1efcb0aa515da59a83230791bd780d505b0c98e
Lubos Balat <lubos.balat@interway.sk>
2025-11-26 15:59:43 +0100

    Merge branch 'main' into feature/57793-jakarta-namespace-migration

--- ant/jakarta-migration.sh
+++ ant/jakarta-migration.sh
@@ -11,3 +11,16 @@ git pull origin feature/57793-jakarta-test-src
 cd ../webjetcms/ant
 echo "migrating to jakarta namespace"
 ant -f jakarta-migration.xml
+
+cd ../../webjetcms_jakarta
+cd src/main/webapp/admin/v9
+npm install
+rm -rf dist
+npm run prod
+
+cd ../../../../..
+cd src/test/webapp
+npm install
+
+cd ../../..
+cd ../webjetcms/ant
