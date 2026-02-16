1dcb6decd28c9c834bedd596f474a8411b700f64
Lubos Balat <lubos.balat@interway.sk>
2026-02-16 10:11:25 +0100

    [57793] feat: base migration scripts

--- ant/jakarta-migration.sh
+++ ant/jakarta-migration.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-# migrate to jakarta namespace, you need to have ../../webjetcms_jakarta folder checkd out with branch feature/57793-jakarta-test-src
 
-cd ../../webjetcms_jakarta
-
-echo "reseting local changes"
-git checkout feature/57793-jakarta-test-src
-git reset --hard
-git pull origin feature/57793-jakarta-test-src
-
-cd ../webjetcms/ant
 echo "migrating to jakarta namespace"
 ant -f jakarta-migration.xml
-
-cd ../../webjetcms_jakarta
-cd src/main/webapp/admin/v9
-npm install
-rm -rf dist
-npm run prod
-
-cd ../../../../..
-cd src/test/webapp
-npm install
-
-cd ../../..
-cd ../webjetcms/ant
