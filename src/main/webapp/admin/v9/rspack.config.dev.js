546433357513d9ec443547bfe9a2e08d13ff2cbb
Lubos Balat <lubos.balat@interway.sk>
2026-04-15 14:34:03 +0200

    [58417] feat: split JS/PUG build

--- src/main/webapp/admin/v9/rspack.config.dev.js
+++ src/main/webapp/admin/v9/rspack.config.dev.js
@@ -5,6 +5,11 @@ const { merge } = require("webpack-merge");
 module.exports = merge(common, {
     mode: "development",
     devtool: 'source-map',
+    output: {
+        //no contenthash in dev mode - prevents PUG recompilation on every JS change (watch mode)
+        filename: "js/[name].js",
+        chunkFilename: 'js/[id].js',
+    },
     plugins: [
         new rspack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify("development"),
