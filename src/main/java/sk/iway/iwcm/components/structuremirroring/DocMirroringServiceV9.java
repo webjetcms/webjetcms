db032d130c81e4b2a9a595c8ebdda097f3cb122a
Lubos Balat <lubos.balat@interway.sk>
2026-05-11 16:17:07 +0200

    feat: update dependencies, dependency-check

--- src/main/java/sk/iway/iwcm/components/structuremirroring/DocMirroringServiceV9.java
+++ src/main/java/sk/iway/iwcm/components/structuremirroring/DocMirroringServiceV9.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import sk.iway.iwcm.Constants;
 import sk.iway.iwcm.LabelValueDetails;
