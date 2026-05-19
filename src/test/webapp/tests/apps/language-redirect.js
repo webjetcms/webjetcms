167d7561c42cb4bdbb9bd93d9a337a9f9aefe498
Lubos Balat <lubos.balat@interway.sk>
2026-05-19 10:39:13 +0200

    [58497] feat: e2e tests

--- src/test/webapp/tests/apps/language-redirect.js
+++ src/test/webapp/tests/apps/language-redirect.js
@@ -78,6 +78,77 @@ Scenario('redirect to english with multiple languages en;q=0.9,sk;q=0.7', async 
     await seeRedirectedTo(I, '/apps/language-redirect/english.html');
 });
 
+Scenario('appstore params test', async ({ I, DTE, Apps, login }) => {
+    login('admin');
+    Apps.insertApp('Presmerovanie podľa jazyka prehliadača', '#apps-app-language-redirect-title', null, false);
+
+    const defaultParams = {
+        defaultLanguage: 'sk',
+        rootOnly: 'false',
+        respectCookie: 'true',
+        mapping1Lang: '',
+        mapping1Url: '',
+        mapping2Lang: '',
+        mapping2Url: '',
+        mapping3Lang: '',
+        mapping3Url: '',
+        mapping4Lang: '',
+        mapping4Url: '',
+        mapping5Lang: '',
+        mapping5Url: '',
+        mapping6Lang: '',
+        mapping6Url: '',
+        mapping7Lang: '',
+        mapping7Url: '',
+        mapping8Lang: '',
+        mapping8Url: ''
+    };
+
+    I.switchTo();
+    I.switchTo();
+    I.clickCss('.cke_dialog_ui_button_ok');
+
+    await Apps.assertParams(defaultParams);
+
+    Apps.openAppEditor();
+
+    const changedParams = {
+        defaultLanguage: 'sk',
+        rootOnly: 'true',
+        respectCookie: 'false',
+        mapping1Lang: 'sk',
+        mapping1Url: '/apps/language-redirect/slovensky.html',
+        mapping2Lang: '',
+        mapping2Url: '/apps/language-redirect/english.html',
+        mapping3Lang: 'en',
+        mapping3Url: '',
+        mapping4Lang: '',
+        mapping4Url: '',
+        mapping5Lang: '',
+        mapping5Url: '',
+        mapping6Lang: '',
+        mapping6Url: '',
+        mapping7Lang: '',
+        mapping7Url: '',
+        mapping8Lang: '',
+        mapping8Url: ''
+    };
+
+    I.clickCss("#pills-dt-component-datatable-advanced-tab");
+    DTE.clickSwitch('rootOnly_0');
+    DTE.clickSwitch('respectCookie_0'); //uncheck checked by default
+    I.clickCss("#pills-dt-component-datatable-basic-tab");
+    DTE.selectOption("mapping1Lang", "Slovenský");
+    DTE.selectOption("mapping3Lang", "Anglický");
+    DTE.fillField('mapping1Url', changedParams.mapping1Url);
+    DTE.fillField('mapping2Url', changedParams.mapping2Url);
+
+    I.switchTo();
+    I.clickCss('.cke_dialog_ui_button_ok');
+
+    await Apps.assertParams(changedParams);
+});
+
 Scenario('cleanup', ({ I }) => {
     I.clearCookie('lng');
     I.setPlaywrightRequestHeaders({});
