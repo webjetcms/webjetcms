a36801c5feb9d26cc44c5f67fcb30ad9de0084b4
Lubos Balat <lubos.balat@interway.sk>
2026-06-18 11:39:37 +0200

    [58569] feat: persist sessions between restarts

--- src/main/java/sk/iway/aceintegration/SpringConfig.java
+++ src/main/java/sk/iway/aceintegration/SpringConfig.java
@@ -1,5 +1,8 @@
 package sk.iway.aceintegration;
 
+import org.apache.catalina.session.FileStore;
+import org.apache.catalina.session.PersistentManager;
+import org.springframework.boot.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.apache.catalina.connector.Connector;
@@ -31,6 +34,26 @@ public class SpringConfig {
             httpConnector.setPort(80);
             httpConnector.setRedirectPort(443);
             factory.addAdditionalConnectors(httpConnector);
+        };
+    }
+
+    /**
+     * Configures file-based session persistence for embedded Tomcat.
+     * Replaces the old META-INF/context.xml PersistentManager configuration
+     * which is not used by Spring Boot 4.x embedded server.
+     *
+     * <p>Sessions are persisted to ${user.home}/.webjetcms/sessions/ on each
+     * request and restored on server restart, so users stay logged in after
+     * server restarts.</p>
+     */
+    @Bean
+    public TomcatContextCustomizer tomcatSessionPersistenceCustomizer() {
+        return context -> {
+            PersistentManager pm = new PersistentManager();
+            FileStore fs = new FileStore();
+            fs.setDirectory(System.getProperty("user.home") + "/.webjetcms/sessions");
+            pm.setStore(fs);
+            context.setManager(pm);
         };
     }
 
