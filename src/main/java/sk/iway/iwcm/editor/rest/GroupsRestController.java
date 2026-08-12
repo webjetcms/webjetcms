11a46ae7bec4d1573984622d89d879c9c510fb70
WebJET CMS <99477108+webjetcms@users.noreply.github.com>
2026-08-12 13:25:34 +0200

    feature/58317-15-multiweb-rename-domain (#280)
    * [58317-15] feat: multiweb - allow to rename domain
    
    * [58317-15] feat: multiweb - check domain name duplicity
    
    * [58317-15] fix: permissions in cloud installName
    
    * [58317-15] feat: multiweb - allow to rename domain
    
    * [58317-15] feat: multiweb - allow to rename domain
    
    * [58317-15] feat: multiweb - allow to rename domain

--- src/main/java/sk/iway/iwcm/editor/rest/GroupsRestController.java
+++ src/main/java/sk/iway/iwcm/editor/rest/GroupsRestController.java
@@ -219,8 +219,8 @@ public class GroupsRestController extends DatatableRestControllerV2<GroupDetails
 
     @Override
     public void beforeSave(GroupDetails entity) {
-        if (InitServlet.isTypeCloud() && CloudToolsForCore.isControllerDomain()==false) {
-            //force current domain
+        if (InitServlet.isTypeCloud() && "cloud".equals(Constants.getInstallName()) && CloudToolsForCore.isControllerDomain()==false) {
+            //force current domain for WebJET Cloud
             entity.setDomainName(CloudToolsForCore.getDomainName());
         }
         super.beforeSave(entity);
@@ -270,6 +270,14 @@ public class GroupsRestController extends DatatableRestControllerV2<GroupDetails
                     //read systemGroupIds for later use
                     systemGroupIds = groupsDB.getSubgroupsIds(system.getGroupId());
                 }
+            }
+            if (oldGroupDetails != null && entity.getGroupName().equals(oldGroupDetails.getDomainName())) {
+                //if group name is same as domain name, we also update group name to reflect new domain name
+                entity.setGroupName(entity.getDomainName());
+            }
+            if (oldGroupDetails != null && entity.getNavbarName().equals(oldGroupDetails.getDomainName())) {
+                //if group name is same as domain name, we update group navbar too
+                entity.setNavbarName(entity.getDomainName());
             }
         }
 
@@ -478,6 +486,7 @@ public class GroupsRestController extends DatatableRestControllerV2<GroupDetails
         }
 
         int parGroupId = entity.getParentGroupId();
+        boolean domainRenamed = false;
         if ((parGroupId < 1 || Constants.getBoolean("multiDomainEnableNested")) && Tools.isNotEmpty(entity.getDomainName()) && entity.getEditorFields().isForceDomainNameChange())
         {
             if(parGroupId < 1) {
@@ -485,17 +494,24 @@ public class GroupsRestController extends DatatableRestControllerV2<GroupDetails
             }
 
             String groupIds = groupsDB.getSubgroupsIds(entity.getGroupId());
+            domainRenamed = true;
+            forceReloadNewDomainName = true;
 
             Adminlog.add(Adminlog.TYPE_GROUP, "Force domain to subgroups: " + groupIds, entity.getGroupId(), entity.getTempId());
 
-            //updatni groups
-            new SimpleQuery().execute("UPDATE groups SET domain_name=? WHERE group_id IN ("+groupIds+")",entity.getDomainName());
-
-            if (systemGroupIds!=null) {
-                Adminlog.add(Adminlog.TYPE_GROUP, "Force domain to system subgroups: " + systemGroupIds, -1, -1);
-
+            if (InitServlet.isTypeCloud() && oldGroupDetails != null && Tools.isNotEmpty(oldGroupDetails.getDomainName())) {
+                //in multiweb rename domain in all root groups
+                new SimpleQuery().execute("UPDATE groups SET domain_name=? WHERE domain_name=?", entity.getDomainName(), oldGroupDetails.getDomainName());
+            } else {
                 //updatni groups
-                new SimpleQuery().execute("UPDATE groups SET domain_name=? WHERE group_id IN ("+systemGroupIds+")", entity.getDomainName());
+                new SimpleQuery().execute("UPDATE groups SET domain_name=? WHERE group_id IN ("+groupIds+")",entity.getDomainName());
+
+                if (systemGroupIds!=null) {
+                    Adminlog.add(Adminlog.TYPE_GROUP, "Force domain to system subgroups: " + systemGroupIds, -1, -1);
+
+                    //updatni groups
+                    new SimpleQuery().execute("UPDATE groups SET domain_name=? WHERE group_id IN ("+systemGroupIds+")", entity.getDomainName());
+                }
             }
 
             //aktualizuj presmerovania
@@ -648,6 +664,15 @@ public class GroupsRestController extends DatatableRestControllerV2<GroupDetails
         }
 
         if (forceReloadNewDomainName) {
+            if (domainRenamed && InitServlet.isTypeCloud()) {
+                //force reload to new domain after change domain name
+                GroupDetails entityReload = new GroupDetails();
+                entityReload.setDomainName(entity.getDomainName());
+                entityReload.setGroupId(-2);
+                entityReload.setEditorFields(new GroupEditorField());
+                entity = entityReload;
+            }
+
             //novo vytvorena domena, potrebujeme vyvolat reload stranky
             //fejkneme to cez atribut forceDomainNameChange, na ktory uz pocuvame v pug subore
             entity.getEditorFields().setForceDomainNameChange(true);
@@ -667,6 +692,11 @@ public class GroupsRestController extends DatatableRestControllerV2<GroupDetails
     @Override
     public void validateEditor(HttpServletRequest request, DatatableRequest<Long, GroupDetails> target, Identity user, Errors errors, Long id, GroupDetails entity) {
 
+        if (InitServlet.isTypeCloud() && "cloud".equals(Constants.getInstallName()) && CloudToolsForCore.isControllerDomain()==false) {
+            //force current domain for WebJET Cloud
+            entity.setDomainName(CloudToolsForCore.getDomainName());
+        }
+
         if (entity.getGroupId()>0 && GroupsDB.isGroupEditable(user, entity.getGroupId())==false) {
             errors.rejectValue("errorField.groupId", "403", Prop.getInstance().getText("user.rights.no_folder_rights"));
             return;
@@ -681,8 +711,16 @@ public class GroupsRestController extends DatatableRestControllerV2<GroupDetails
 			if (oldGroup == null || oldGroup.getDomainName().equals(CloudToolsForCore.getDomainName())==false)
 			{
 				errors.rejectValue("errorField.domainName", "403", Prop.getInstance().getText("user.rights.no_folder_rights"));
-                return;
+	            return;
 			}
+
+            if (Tools.isNotEmpty(entity.getDomainName()) && oldGroup.getDomainName().equalsIgnoreCase(entity.getDomainName())==false) {
+                int domainCount = new SimpleQuery().forInt("SELECT COUNT(*) FROM groups WHERE LOWER(domain_name)=LOWER(?)", entity.getDomainName());
+                if (domainCount > 0) {
+                    errors.rejectValue("errorField.domainName", "403", Prop.getInstance(request).getText("groupedit.domain_already_exists"));
+                    return;
+                }
+            }
 		}
 
         if ("remove".equals(target.getAction())) return;
